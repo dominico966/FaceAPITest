4e68fce7206bd48bbead083a752f44f9001a1b65
dominico966 <dominico966@gmail.com>
2018-05-17 18:23:54 +0900

    임시 코드변경

--- skuface/src/main/java/com/dominic/skuface/FaceApi.java
+++ skuface/src/main/java/com/dominic/skuface/FaceApi.java
@@ -35,9 +35,10 @@ public class FaceApi {
 
     private static FaceApi instance = null;
 
-    private static final String SERVICE_KEY = "90b274384fc64538923eb77e7f61e020";
+    private static final String SERVICE_KEY = "f4bc7755bb894e229ebbcfa5e824c484";
 
-    private static final String REST_URL = "https://eastasia.api.cognitive.microsoft.com/face/v1.0/detect";
+    //private static final String REST_URL = "https://eastasia.api.cognitive.microsoft.com/face/v1.0/detect";
+    private static final String REST_URL = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
 
     private Vector<Face> faceList = new Vector<>();
 
